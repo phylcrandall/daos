6ccf672f46658b3e7bcf51aa38a0f9733da95267
Brian J. Murrell <brian.murrell@intel.com>
2018-11-18 14:12:47 -0500

    Use subprocess instead of aexpect
    Change-Id: Ib913e6d00ce047d5ad78c89278dd619b41c8817c
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- src/tests/ftest/util/ServerUtils.py
+++ src/tests/ftest/util/ServerUtils.py
@@ -27,9 +27,11 @@ import time
 import subprocess
 import json
 import re
-import time
+import resource
+import signal
+import fcntl
+import errno
 
-import aexpect
 from avocado.utils import genio
 
 sessions = {}
@@ -77,7 +79,6 @@ def runServer(hostfile, setname, basepath, uri_path=None, env_dict=None):
                 if re.match(pat, env_var):
                     env_args += "-x {}='{}' ".format(env_var, env_val)
 
-        initial_cmd = "/bin/sh"
         server_cmd = orterun_bin + " --np {0} ".format(server_count)
         if uri_path is not None:
             server_cmd += "--report-uri {0} ".format(uri_path)
@@ -90,34 +91,55 @@ def runServer(hostfile, setname, basepath, uri_path=None, env_dict=None):
 
         print "Start CMD>>>>{0}".format(server_cmd)
 
-        sessions[setname] = aexpect.ShellSession(initial_cmd)
-        if sessions[setname].is_responsive():
-            sessions[setname].sendline("ulimit -c unlimited")
-            sessions[setname].sendline(server_cmd)
-            timeout = 300
-            start_time = time.time()
-            result = 0
-            expected_data = "Starting Servers\n"
-            while True:
-                pattern = "DAOS I/O server"
-                output = sessions[setname].read_nonblocking(2, 2)
-                match = re.findall(pattern, output)
-                expected_data = expected_data + output
-                result += len(match)
-                if result == server_count or time.time() - start_time > timeout:
-                    print ("<SERVER>: {}".format(expected_data))
-                    if result != server_count:
-                        raise ServerFailed("Server didn't start!")
-                    break
-            print "<SERVER> server started and took %s seconds to start" % \
-                  (time.time() - start_time)
-    except Exception as e:
-        print "<SERVER> Exception occurred: {0}".format(str(e))
+        resource.setrlimit(
+            resource.RLIMIT_CORE,
+            (resource.RLIM_INFINITY, resource.RLIM_INFINITY))
+
+        # TODO: remove shell=True and split server_cmd into a list
+        sessions[setname] = subprocess.Popen(server_cmd,
+                                             stdout=subprocess.PIPE,
+                                             stderr=subprocess.PIPE,
+                                             shell=True)
+        fd = sessions[setname].stdout.fileno()
+        fl = fcntl.fcntl(fd, fcntl.F_GETFL)
+        fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
+        timeout = 600
+        start_time = time.time()
+        result = 0
+        pattern = "DAOS I/O server"
+        expected_data = "Starting Servers\n"
+        while True:
+            output = ""
+            try:
+                output = sessions[setname].stdout.read()
+            except IOError as excpn:
+                if excpn.errno != errno.EAGAIN:
+                    raise excpn
+                continue
+            match = re.findall(pattern, output)
+            expected_data += output
+            result += len(match)
+            if not output or result == server_count or \
+               time.time() - start_time > timeout:
+                print("<SERVER>: {}".format(expected_data))
+                if result != server_count:
+                    raise ServerFailed("Server didn't start!")
+                break
+        print "<SERVER> server started and took %s seconds to start" % \
+              (time.time() - start_time)
+    except Exception as excpn:
+        print "<SERVER> Exception occurred: {0}".format(str(excpn))
         # we need to end the session now -- exit the shell
         try:
-            sessions[setname].sendline("exit")
-            # for good measure, try to close it
-            sessions[setname].close()
+            sessions[setname].send_signal(signal.SIGINT)
+            time.sleep(5)
+            # get the stderr
+            error = sessions[setname].stderr.read()
+            if sessions[setname].poll() is None:
+                sessions[setname].kill()
+            retcode = sessions[setname].wait()
+            print "<SERVER> server start return code: {}\n" \
+                  "stderr:\n{}".format(retcode, error)
         except KeyError:
             pass
         raise ServerFailed("Server didn't start!")
@@ -133,17 +155,17 @@ def stopServer(setname=None, hosts=None):
     try:
         if setname == None:
             for k, v in sessions.items():
-                v.sendcontrol("c")
-                v.sendcontrol("c")
-                # we need to end the session now -- exit the shell
-                v.sendline("exit")
-                v.close()
+                v.send_signal(signal.SIGINT)
+                time.sleep(5)
+                if v.poll() == None:
+                    v.kill()
+                v.wait()
         else:
-            sessions[setname].sendcontrol("c")
-            sessions[setname].sendcontrol("c")
-            # we need to end the session now -- exit the shell
-            sessions[setname].sendline("exit")
-            sessions[setname].close()
+            sessions[setname].send_signal(signal.SIGINT)
+            time.sleep(5)
+            if sessions[setname].poll() == None:
+                sessions[setname].kill()
+            sessions[setname].wait()
         print "<SERVER> server stopped"
     except Exception as e:
         print "<SERVER> Exception occurred: {0}".format(str(e))
